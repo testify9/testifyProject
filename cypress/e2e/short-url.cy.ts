d0215a300df41fd65186596297ece4105137ef4e
SK <sk@example.com>
2022-06-19 21:40:01 +0200

    lll

--- cypress/e2e/short-url.cy.ts
+++ cypress/e2e/short-url.cy.ts
@@ -55,39 +55,39 @@ describe('Short Url Tests', function () {
     });
   
     it('New short url with same sourcePath cannot be created', { scrollBehavior: 'top' }, () => {
-      createShortUrl('cypress_test_sourcePath', 'cypress_test_targetPath');
+      createShortUrl('cypress_test_sourcePath_1', 'cypress_test_targetPath_1');
   
       cy.get('[data-cy=create-shorturl]').click('top');
 
       cy.get('[data-cy=shorturl-form]').should('be.visible');
   
       // eslint-disable-next-line cypress/no-unnecessary-waiting
-      cy.wait(1000);
+      cy.wait(3000);
 
       cy.get('[data-cy=source-input]').should('be.visible');
   
       cy.get('[data-cy=source-input]')
         .find('input[placeholder="Source Path"]', { includeShadowDom: true })
         .click()
-        .type('cypress_test_sourcePath', { force: true });
+        .type('cypress_test_sourcePath_1', { force: true });
       cy.get('[data-cy=target-input]')
         .find('input[placeholder="Target Path"]', { includeShadowDom: true })
         .click()
-        .type('cypress_test_targetPath', { force: true });
+        .type('cypress_test_targetPath_1', { force: true });
   
       // eslint-disable-next-line cypress/no-unnecessary-waiting
-      cy.wait(1000);
+      cy.wait(3000);
   
       cy.get('[data-cy=submit]').find('rb-button').should('have.attr', 'disabled');
       cy.get('[data-cy=cancel]').click('top');
   
-      deleteShortUrl('cypress_test_sourcePath');
+      deleteShortUrl('cypress_test_sourcePath_1');
     });
   
     it('Edited text changes', { scrollBehavior: 'top' }, () => {
-      createShortUrl('cypress_test_sourcePath', 'cypress_test_targetPath');
+      createShortUrl('cypress_test_sourcePath_2', 'cypress_test_targetPath_2');
   
-      cy.contains('cypress_test_targetPath')
+      cy.contains('cypress_test_targetPath_2')
         .parents('[data-cy=shorturl-display]')
         .find('[data-cy=more]')
         .click('top')
@@ -101,7 +101,7 @@ describe('Short Url Tests', function () {
         .focus()
         .clear()
         .click()
-        .type('cypress_test_targetPath_edit', { force: true });
+        .type('cypress_test_targetPath_2_edit', { force: true });
   
       // eslint-disable-next-line cypress/no-unnecessary-waiting
       cy.wait(1000);
@@ -109,27 +109,27 @@ describe('Short Url Tests', function () {
       cy.get('[data-cy=submit]').should('be.visible')
       cy.get('[data-cy=submit]').click('top');
   
-      cy.contains('cypress_test_targetPath_edit')
+      cy.contains('cypress_test_targetPath_2_edit')
         .parents('[data-cy=shorturl-display]')
         .find('[data-cy=updatedBy]')
         .should('contain', Cypress.env('auth0_username'));
   
-      deleteShortUrl('cypress_test_sourcePath');
+      deleteShortUrl('cypress_test_sourcePath_2');
     });
   
     it('Searching for sourcePath displays correct card', { scrollBehavior: 'top' }, () => {
-      createShortUrl('cypress_test_sourcePath', 'cypress_test_targetPath');
+      createShortUrl('cypress_test_sourcePath_3', 'cypress_test_targetPath_3');
   
       cy.get('rb-search-filter-bar')
         .find('input', { includeShadowDom: true })
         .click({ force: true })
-        .type('cypress_test_sourcePath', { force: true });
+        .type('cypress_test_sourcePath_3', { force: true });
   
       // eslint-disable-next-line cypress/no-unnecessary-waiting
       cy.wait(1000);
   
-      cy.get('cms-list-view').should('contain', 'cypress_test_sourcePath');
+      cy.get('cms-list-view').should('contain', 'cypress_test_sourcePath_3');
   
-      deleteShortUrl('cypress_test_sourcePath');
+      deleteShortUrl('cypress_test_sourcePath_3');
     });
   });
