dd897359904611e41bed949368d396f1e36afffa
SK <sk@example.com>
2022-06-19 19:25:54 +0200

    move config

--- cypress.config.js
+++ cypress.config.js
@@ -9,9 +9,9 @@ module.exports = defineConfig({
     // We've imported your old cypress plugins here.
     // You may want to clean this up later by importing these.
     setupNodeEvents(on, config) {
-      return require('./plugins/index.js')(on, config)
+      return require('./cypress/plugins/index.js')(on, config)
     },
-    specPattern: './e2e/**/*.cy.{js,jsx,ts,tsx}',
-    supportFile: './support/index.ts',
+    specPattern: './cypress/e2e/**/*.cy.{js,jsx,ts,tsx}',
+    supportFile: './cypress/support/index.ts',
   },
 })
